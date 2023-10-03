587bc95aacbd6166b85f5e5f66f0e0cea766d606
caojiekiz <caojiekiz@gmail.com>
2023-10-03 21:59:24 +0900

    add

---
+++ spring/spring_20_aop_advice_type/src/main/java/com/itheima/config/SpringConfig.java
@@ -0,0 +1,14 @@
+package com.itheima.config;
+
+
+import org.springframework.context.annotation.*;
+
+
+@Configuration
+@ComponentScan("com.itheima")
+@EnableAspectJAutoProxy
+
+public class SpringConfig {
+
+
+}
